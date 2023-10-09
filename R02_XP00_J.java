838402cc2e02adfb14663e090a2bb975062a1e60
MojavePatroller <70491745+MojavePatroller@users.noreply.github.com>
2023-10-08 23:10:53 -0400

    Vulnerable code 2

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -3,12 +3,10 @@ Vulnerability 2
 XP00-J. Do not ignore values returned by methods Given the non-compliant code below:
 */
 
-public void delete File() {
-    
-    File someFile = new File(”someFileName.txt”);
-    // Do something with someFile
-    if ( !someFile.delete()) {
-        // Handle failure to delete the file
-    }
-
+public void deleteFile(){
+ 
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  someFile.delete();
+ 
 }
