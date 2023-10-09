acf7830cc7f9c60a5b0286be7324dee578a80477
MojavePatroller <70491745+MojavePatroller@users.noreply.github.com>
2023-10-08 21:50:47 -0400

    Modified vulerable code 3

---
+++ R03_NUM03_J.java
@@ -0,0 +1,8 @@
+/* 
+Vulnerability 3
+NUM03-J. Use integer types that can fully represent the possible range of unsigned data
+}*/
+
+public static long getInteger (DataInputStream is) throws IOException {
+    return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+}
