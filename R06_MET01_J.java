bfe7ea53d872a2b132f4d8d3a327e46dc93b9791
MojavePatroller <70491745+MojavePatroller@users.noreply.github.com>
2023-10-08 22:41:40 -0400

    Update R06_MET01_J.java
    Correction on modified code for vulnerability 5

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -3,12 +3,14 @@ Vulnerable code 5
 MET01-J. Never use assertions to validate method arguments 
 */
 
-public static int getAbsAdd(int x, int y) {
-assert x != Integer.MIN_VALUE;
-assert y != Integer.MIN VALUE;
-int absX = Math.abs(x);
-int absY = Math.abs(y);
-assert (absX <= Integer.MAXVALUE - absY);
-return absX + absY;
+public static int getAbsAdd (int x, int y) {
+    if (x == Integer .MIN_VALUE || y == Integer .MIN_VALUE) {
+        throw new IllegalArgumentException();
+    }
+    int absX = Math.abs(x);
+    int absY = Math.abs(y);
+    if (absX > Integer.MAXVALUE - absY) {
+        throw new IllegalArgumentException();
+    }
+    return absX + absY;
 }
-//Usage : getAbsAdd(Integer.MIN_VALUE, 1);
