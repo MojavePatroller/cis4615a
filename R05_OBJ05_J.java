cddfad0e1760944d43359396dd42c3bc86d1b558
MojavePatroller <70491745+MojavePatroller@users.noreply.github.com>
2023-10-08 22:53:16 -0400

    Vulnerable code 7

---
+++ R05_OBJ05_J.java
@@ -0,0 +1,19 @@
+/*
+Rule 05. Object Orientation (OBJ) 
+OBJ05-J. Do not return references to private mutable class members  
+ */
+
+ class MutableClass {
+    private Date[] date;
+   
+    public MutableClass() {
+      date = new Date[20];
+      for (int i = 0; i < date.length; i++) {
+        date[i] = new Date();
+      }
+    }
+   
+    public Date[] getDate() {
+      return date; // Or return date.clone()
+    }
+  }
